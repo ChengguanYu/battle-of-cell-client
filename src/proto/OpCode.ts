9e253e8f4e28a0987841ab46857c60120df56a49
weiyc <cswei1024@gmail.com>
2026-07-24 14:35:42 +0800

    fix: sync OuterOpcode with server EntryRoom shift

--- src/proto/OpCode.ts
+++ src/proto/OpCode.ts
@@ -3,15 +3,17 @@ export const OpCode = {
   ClientFrame: 134227729,
   EntryHomeReq: 268445457,
   EntryHomeResp: 402663185,
+  EntryRoomReq: 268445458,
+  EntryRoomResp: 402663186,
   MetaData: 134227730,
-  PlayerLeaveRoomReq: 268445458,
-  PlayerLeaveRoomResp: 402663186,
-  PlayerMatchReq: 268445459,
-  PlayerMatchResp: 402663187,
+  PlayerLeaveRoomReq: 268445459,
+  PlayerLeaveRoomResp: 402663187,
+  PlayerMatchReq: 268445460,
+  PlayerMatchResp: 402663188,
   RespError: 134227731,
   ServerFrame: 134227732,
-  SessionHeartbeatPing: 268445460,
-  SessionHeartbeatPong: 402663188,
+  SessionHeartbeatPing: 268445461,
+  SessionHeartbeatPong: 402663189,
 } as const
 
 export type OpCode = (typeof OpCode)[keyof typeof OpCode]
